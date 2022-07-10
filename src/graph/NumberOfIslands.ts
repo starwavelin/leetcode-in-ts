bd1082b8f5c8573be11bfde9cd66fb0b4ded1031
“starwavelin” <benjaminxianlin@gmail.com>
2022-07-10 12:23:37 -0400

    feat: LC 200 BFS sol up

--- src/graph/NumberOfIslands.ts
+++ src/graph/NumberOfIslands.ts
@@ -7,40 +7,85 @@
  * meta        : tag-graph, tag-matrix, tag-bfs, tag-dfs
  ***************************************************************************/
 
-export function numIslands(grid: string[][]): number {
-    // m - num of rows, n - num of cols
-    const m = grid.length;
-    const n = grid[0].length;
+export class NumberOfIslands {
+    numIslands(grid: string[][]): number {
+        // m - num of rows, n - num of cols
+        const m = grid.length;
+        const n = grid[0].length;
+    
+        let res = 0;
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < n; j++) {
+                if (grid[i][j] === '1') {                
+                    this.dfs(grid, i, j, m, n);
+                    res++;
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    dfs = (grid: string[][], row: number, col: number, m: number, n: number) => {
+        // 递归的退出条件
+        if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] === '0') {
+            return;
+        }
+    
+        grid[row][col] = '0'; // dye the cell
+        const dx = [-1, 1, 0, 0];
+        const dy = [0, 0, -1, 1];
+        for (let i = 0; i < 4; i++) {
+            const rowNum = row + dx[i];
+            const colNum = col + dy[i];
+            this.dfs(grid, rowNum, colNum, m, n);
+        }
+    }
 
-    let res = 0;
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] === '1') {
-                res++;
-                dfs(grid, i, j, m, n);
+    numIslandsSolBfs(grid: string[][]): number {
+        const m = grid.length;
+        const n = grid[0].length;
+    
+        let res = 0;
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < n; j++) {
+                if (grid[i][j] === '1') {                
+                    this.bfs(grid, i, j, m, n);
+                    res++;
+                }
+            }
+        }
+        
+        return res;
+    }
+
+    bfs = (grid: string[][], row: number, col: number, m: number, n: number): void => {
+        // 1. 先染色
+        grid[row][col] = '0';
+
+        // 2. Initialization
+        const q: number[] = [];
+        const pos = row * n + col;
+        q.push(pos);
+
+        // 3. dx dy
+        const dx = [-1, 1, 0, 0];
+        const dy = [0, 0, -1, 1];
+
+        // 4. 经典BFS
+        while (q.length) {
+            const cur = q.shift() as number; // the current position
+
+            for (let i = 0; i < 4; i++) {
+                const rowNum = Math.trunc(cur / n) + dx[i];
+                const colNum = cur % n + dy[i];
+
+                //继续BFS染色的合理条件
+                if (rowNum >= 0 && rowNum < m && colNum >= 0 && colNum < n && grid[rowNum][colNum] === '1') {
+                    grid[rowNum][colNum] = '0';
+                    q.push(rowNum * n + colNum);
+                } 
             }
         }
     }
-    
-    return res;
-}
-
-const dfs = (grid: string[][], row: number, col: number, m: number, n: number) => {
-    if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] === '0') {
-        return;
-    }
-
-    grid[row][col] = '0'; // dye the cell
-    const dx = [-1, 1, 0, 0];
-    const dy = [0, 0, -1, 1];
-    for (let i = 0; i < 4; i++) {
-        const rowNum = row + dx[i];
-        const colNum = col + dy[i];
-        dfs(grid, rowNum, colNum, m, n);
-    }
-}
-
-
-export function numIslandsSolBfs(grid: string[][]): number {
-    return 0; // just keep the compiler happy
 }
--- tests/graph/NumberOfIslands.test.ts
+++ tests/graph/NumberOfIslands.test.ts
@@ -1,13 +1,15 @@
-import { numIslands } from '../../src/graph/NumberOfIslands';
+import { NumberOfIslands } from '../../src/graph/NumberOfIslands';
 
 describe('Test NumberOfIslands Sol DFS', () => {
+    const instance = new NumberOfIslands();
+
     test('Test - Ex 1', () => {
         const grid = [
             ['1', '1', '0'],
             ['1', '1', '0'],
             ['0', '0', '1']
         ]
-        expect(numIslands(grid)).toBe(2);
+        expect(instance.numIslands(grid)).toBe(2);
     });
 
     test('Test - Ex 2', () => {
@@ -17,6 +19,26 @@ describe('Test NumberOfIslands Sol DFS', () => {
             ['0', '0', '1', '0', '0'],
             ['0', '0', '0', '1', '1'],
         ]
-        expect(numIslands(grid)).toBe(3);
+        expect(instance.numIslands(grid)).toBe(3);
+    });
+
+
+    test('Test BFS - Ex 1', () => {
+        const grid = [
+            ['1', '1', '0'],
+            ['1', '1', '0'],
+            ['0', '0', '1']
+        ]
+        expect(instance.numIslandsSolBfs(grid)).toBe(2);
+    });
+
+    test('Test BFS - Ex 2', () => {
+        const grid = [
+            ['1', '1', '0', '0', '0'],
+            ['1', '1', '0', '0', '0'],
+            ['0', '0', '1', '0', '0'],
+            ['0', '0', '0', '1', '1'],
+        ]
+        expect(instance.numIslandsSolBfs(grid)).toBe(3);
     });
 })
