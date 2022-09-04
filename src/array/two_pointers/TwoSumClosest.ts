405862bfee265dea24e9496a82c3ec3910eaad7a
“starwavelin” <benjaminxianlin@gmail.com>
2022-09-04 17:46:51 -0400

    feat: TwoSumClosest Scenario 1 and solution up

---
+++ src/array/two_pointers/TwoSumClosest.ts
@@ -0,0 +1,52 @@
+/***************************************************************************
+ * Problem No. : 
+ * Problem Name: Two Sum Closest
+ * Date        : September 04, 2022
+ * Author      : @codingbro
+ *
+ * meta        : tag-two-pointers, tag-array, tag-sort
+ ***************************************************************************/
+
+export class TwoSumClosest {
+
+    /**
+     * Description:
+     *  Given an array of integers, and a target value k, 
+     *  find the sum of two numbers that is closest to k
+     *  return the sum.
+     * input: [14, 5, -1, 7, 9] k = 11
+     * after sorting [-1, 5, 7, 9, 14]
+        output: 12 (contributed by 5 and 7) because 12 and 11’s difference is 1
+     */
+    findSum(nums: number[], k: number): number {
+        const n = nums.length;
+        nums.sort((a, b) => a-b);
+        let i = 0, j = n-1;
+        let sum = nums[i] + nums[j];
+        let minDiff = Math.abs(sum - k);
+        while (i < j) {
+            let localSum = nums[i] + nums[j];
+            let localDiff = Math.abs(localSum - k);
+            if (localDiff < minDiff) {
+                minDiff = localDiff;
+                sum = localSum;
+            }
+            if (localSum > k) {
+                j--;
+            } else if (localSum <= k) {
+                i++;
+            }
+        }
+        return sum;
+    }
+
+}
+
+
+/**
+ * Test
+ */
+const t1 = new TwoSumClosest();
+// console.log(t1.findSum([14, 5, -1, 7, 9], 11)); // return 12
+// [1, 3, 4, 5, 7, 8]
+console.log(t1.findSum([7, 5, 3, 8, 4, 1], 13)); // return 13
---
+++ tests/array/two_pointers/TwoSumClosest.test.ts
@@ -0,0 +1,29 @@
+import { TwoSumClosest } from '../../../src/array/two_pointers/TwoSumClosest';
+
+describe('Test TwoSumClosest', () => {
+    const ins = new TwoSumClosest();
+
+    describe('Test - Scenario 1', () => {
+        test('Ex 1', () => {
+            const nums = [7, 5, 3, 8, 4, 1];
+            expect(ins.findSum(nums, 13)).toBe(13);
+        });
+
+        test('Ex 2', () => {
+            const nums = [14, 5, -1, 7, 9];
+            expect(ins.findSum(nums, 11)).toBe(12);
+        });
+    });
+
+    // describe('Test - Sol 2', () => {
+    //     test('Ex 1', () => {
+    //         const nums = [8, 6, 4, 5, 10];
+    //         expect(instance.sortArrayByParitySol2(nums)).toEqual([8, 6, 4, 10, 5]);
+    //     });
+
+    //     test('Ex 2', () => {
+    //         const nums = [8, 6, 4, 5, 7, 9, 10];
+    //         expect(instance.sortArrayByParitySol2(nums)).toEqual([8, 6, 4, 10, 7, 9, 5]);
+    //     });
+    // });
+})
