a584f81ebba6991991380a8ab3ad447e68c5b1a9
“starwavelin” <benjaminxianlin@gmail.com>
2023-04-23 16:30:57 -0400

    chore: polish LC 15 ThreeSUm

--- src/array/two_pointers/ThreeSum.js
+++ src/array/two_pointers/ThreeSum.js
@@ -39,7 +39,10 @@ var threeSumVer3 = function(nums, target) {
 console.log(threeSumVer3([-1, 0, 1, 2, -1, -4], 3)); // should get [[0,1,2]]
 console.log(threeSumVer3([0, 0, 0], 3)); // should get []
 console.log(threeSumVer3([-1,0,1,2,-1,-4,-2,-3,3,0,4], 3)); // should get
-    // [[-4, 3, 4], [-3, 2, 4], [-2, 2, 3], [-1, 0, 4], [-1, 1, 3], [0, 0, 3], [0, 1, 2]]
+    // [[-4, 3, 4], [-3, 2, 4], [-2, 1, 4], [-2, 2, 3], [-1, 0, 4], [-1, 1, 3], [0, 0, 3], [0, 1, 2]]
+console.log(threeSumVer3([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6], 0)); // should get
+    // [[-4,-2,6],[-4,0,4],[-4,1,3],[-4,2,2],[-2,-2,4],[-2,0,2]]
+
 console.log('DEBUG: END');
 
 
