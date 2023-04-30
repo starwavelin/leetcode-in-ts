19f834e0ff647ca32d77353c279aca1443df4963
“starwavelin” <benjaminxianlin@gmail.com>
2023-04-30 12:03:46 -0400

    feat: implement minHeap in JS

---
+++ src/heap/MinHeap.js
@@ -0,0 +1,72 @@
+
+/**
+ * Heapify过程的本质就是父节点和子节点在有必要的情况下,进行交换
+ * @param {*} array 
+ * @param {*} index 
+ */
+function minHeapify(array, index) {
+    const n = array.length;
+    let childIndex;
+
+    for (; index * 2 < n; index = childIndex) {
+        childIndex = index * 2;
+
+        // Choose the smaller one of the two (left, right) children
+        if (childIndex !== n-1 && array[childIndex] > array[childIndex+1]) {
+            childIndex++;
+        }
+
+        if (array[index] > array[childIndex]) {
+            [array[index], array[childIndex]] = [array[childIndex], array[index]];
+        } else {
+            break;
+        }
+    }
+}
+
+/**
+ * The time complexity of buildHeap is O(n), 
+ *  reference: https://github.com/GingerBear/IS-Job-Hunting/issues/10
+ * @param {*} array 
+ * @returns A heap stored in an array
+ */
+function buildHeap(array) {
+    const heap = [undefined, ...array];
+    const n = heap.length;
+    for (let i = Math.floor(n / 2); i >= 1; i--) {
+        minHeapify(heap, i);
+    }
+    return heap;
+}
+
+function remove(heap) {
+    if (heap.length === 1) { 
+        /* when the heap array has only one element, 
+            which is the beginning undefined one, we don't remove it */
+        throw new Error('The heap is empty, cannot remove anything from it');
+    }
+
+    const min = heap[1];    
+
+    // Maintain the heap
+    const last = heap.pop();
+    if (heap.length > 1) {
+        heap[1] = last;
+        minHeapify(heap, 1);
+    }
+
+    return min;
+}
+
+
+
+/**
+ * Testing
+ */
+const origArray1 = [11, 12, 13, 10, 9, 5, 1, 15];
+let heap1 = buildHeap(origArray1);
+console.log(heap1);  // expect [ undefined, 1, 9, 5, 10, 12, 11, 13, 15 ]
+console.log(remove(heap1)); // expect 1
+console.log(heap1);
+console.log(remove(heap1)); // expect 5
+console.log(heap1);
---
+++ src/heap/MinHeapChatGPT.js
@@ -0,0 +1,90 @@
+/**
+ * This MinHeap class is written by ChatGPT,
+ *  I can just use my own MinHeap class (customized from ChatGPT)
+ *  to satisfy the LeetCode problem goals. 
+ *  That'd be good enough.
+ */
+class MinHeapChatGPT {
+    constructor() {
+        this.heap = [];
+    }
+
+    // Helper function to swap two elements in the heap
+    swap(i, j) {
+        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+    }
+
+    // Helper function to maintain the heap property starting from index i
+    heapify(i) {
+        const n = this.heap.length;
+        let temp = this.heap[i];
+        let childIndex;
+
+        for (; i * 2 < n; i = childIndex) {
+            childIndex = i * 2;
+
+            // Choose the smaller of the two (left, right) children
+            if (childIndex != n - 1 && this.heap[childIndex][1] > this.heap[childIndex + 1][1]) {
+                childIndex++;
+            }
+
+            if (temp[1] > this.heap[childIndex][1]) {
+                this.heap[i] = this.heap[childIndex];
+            } else {
+                break;
+            }
+        }
+
+        this.heap[i] = temp;
+    }
+
+    // Build a heap from an array of [number, frequency] pairs
+    buildHeap(array) {
+        this.heap = array.slice();
+        const n = this.heap.length;
+
+        // Start from the last non-leaf node and heapify each node in reverse order
+        for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+            this.heapify(i);
+        }
+    }
+
+    // Remove the minimum element from the heap
+    removeMin() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+
+        const min = this.heap[0];
+        const last = this.heap.pop();
+
+        if (this.heap.length > 0) {
+            this.heap[0] = last;
+            this.heapify(0);
+        }
+
+        return min;
+    }
+
+    // Get the minimum element from the heap without removing it
+    peek() {
+        return this.heap[0];
+    }
+
+    // Get the size of the heap
+    size() {
+        return this.heap.length;
+    }
+
+    // Check if the heap is empty
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+}
+
+function createMinHeapFromFrequencyMap(freqMap) {
+    const array = Array.from(freqMap);
+    const minHeap = new MinHeapChatGPT();
+    minHeap.buildHeap(array);
+    return minHeap;
+}
