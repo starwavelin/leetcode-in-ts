94162c8c0141be904f8c39fea0bfd5f60d4d603c
“starwavelin” <benjaminxianlin@gmail.com>
2023-07-07 16:21:25 -0400

    feat: LC 4 binary search solution up

--- src/binary_search/MedianOfTwoSortedArrays.js
+++ src/binary_search/MedianOfTwoSortedArrays.js
@@ -43,10 +43,94 @@ const merge = (nums1, nums2) => {
 
 
 /**
- * Solution 2
- * @param {*} nums1 
- * @param {*} nums2 
+ * Solution 2 -- NeetCode's approach
+ * When the combined arry has an odd length, the median locates at the minimum of the two mins of the right partitions
  */
 var findMedianOfTwoSortedArraysSol2 = function(nums1, nums2) {
+    // If nums1 is larger than nums2, swap them to ensure n1 is smaller than n2.
+    if (nums1.length > nums2.length) {
+        [nums1, nums2] = [nums2, nums1];
+    }
+
+    const n1 = nums1.length;
+    const n2 = nums2.length;
+    const total = n1 + n2;
+    const half = Math.floor(total / 2);
     
+    let l = 0;
+    let r = n1 - 1;
+    while (true) {
+        let mid1 = Math.floor((l + r) / 2);   // the mid index of the first (smaller) array
+        let mid2 = half - mid1 - 2;        // the mid index of the 2nd array shall be half minus the mid1, and also b/c the array is 0-indexed, minus the two zeros
+        
+        let maxLeft1 = (mid1 < 0) ? Number.MIN_SAFE_INTEGER : nums1[mid1];
+            // The typical index of the maximum of the left partition of nums1 is the calculated mid1,
+            // so the violating condition is that when mid1 is less than 0
+        let minRight1 = (mid1 + 1 >= n1) ? Number.MAX_SAFE_INTEGER : nums1[mid1+1];
+            // The typical index of the minimum of the right partition of nums1 is the calculated mid1+1,
+            // so the violating condition is that when mid1+1 equals the length of nums1
+        
+        let maxLeft2 = (mid2 < 0) ? Number.MIN_SAFE_INTEGER : nums2[mid2];
+        let minRight2 = (mid2 + 1 >= n2) ? Number.MAX_SAFE_INTEGER : nums2[mid2+1];
+        
+        if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+            if ((n1 + n2) % 2 == 0) {
+                return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2;
+            } else {
+                return Math.min(minRight1, minRight2);
+            }
+        } else if (maxLeft1 > minRight2 ) {
+            r = mid1 - 1; // the current mid1 should be included into the left partition
+                // nums2 should have more numbers included into the left partition -- 在nums1中取数取多了,r要往下减
+        } else { // condition: maxLeft2 > minRight1
+            l = mid1 + 1; // the current mid1 should be included into the left partition -- 在nums1中取数取少了,l要往上加
+        }
+    }
 };
+
+
+/**
+ * The solution below is a re-write from devanshu's solution on LeetCode
+ * Basically in devenshu's solutoin, the median is the max of the two maximum values in the left partitions 
+ */
+var findMedianOfTwoSortedArraysSol3 = function(nums1, nums2) {
+    // If nums1 is larger than nums2, swap them to ensure n1 is smaller than n2.
+    if (nums1.length > nums2.length) {
+        [nums1, nums2] = [nums2, nums1];
+    }
+
+    const n1 = nums1.length;
+    const n2 = nums2.length;
+    
+    let l = 0;
+    let r = n1;
+    while (true) {
+        let mid1 = Math.floor((l + r) / 2);
+        let mid2 = Math.floor((n1 + n2 + 1) / 2 - mid1);
+        
+        let maxLeft1 = (mid1 === 0) ? Number.MIN_SAFE_INTEGER : nums1[mid1-1]; 
+            // The typical index of the maximum of the left partition of nums1 is the calculated mid1 minus 1
+            // That's why when mid1 equals 0 we want to set the max of the left partition of nums1 array to be MIN_NUM
+        let minRight1 = (mid1 === n1) ? Number.MAX_SAFE_INTEGER : nums1[mid1];
+            // The typical index of the minumum of the right partition of nums1 is the calculated mid1
+            // That's why the illegal condition starts when mid1 equals nums1's length
+        
+        let maxLeft2 = (mid2 === 0) ? Number.MIN_SAFE_INTEGER : nums2[mid2-1];
+        let minRight2 = (mid2 === n2) ? Number.MAX_SAFE_INTEGER : nums2[mid2];
+        
+        if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+            if ((n1 + n2) % 2 === 0) {
+                return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2;
+            } else {
+                return Math.max(maxLeft1, maxLeft2);
+            }
+        } 
+        // Handling offsets
+        else if (maxLeft1 > minRight2) {
+            r = mid1 - 1;
+        } 
+        else {
+            l = mid1 + 1;
+        }
+    }
+}
