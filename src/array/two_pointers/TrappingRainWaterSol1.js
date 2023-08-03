355e920673341b541d89682af55bf96bd9a4e701
“starwavelin” <benjaminxianlin@gmail.com>
2023-08-03 17:57:08 -0400

    feat: LC 42 Solution 2 Two-pointer solution up

--- src/array/two_pointers/TrappingRainWaterSol1.js
+++ src/array/two_pointers/TrappingRainWaterSol1.js
@@ -4,7 +4,7 @@
  * Date        : August 3, 2023
  * Author      : @codingbro
  *
- * meta        : tag-array, tag-math
+ * meta        : tag-array, tag-math, tag-dp
  ***************************************************************************/
 
 /**
@@ -41,3 +41,31 @@ var trap = function(height) {
 // Tests
 console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1])); // 6
 console.log(trap([4,2,0,3,2,5])); // 9
+
+
+/**
+ * We can also optimize the solution above by just 
+ *  traversing the array twice, instead of 3 times.
+ * And BTW, these two solutions are dynamic programming type solution
+ */
+
+var trapSolDP = function(height) {
+    const n = height.length;
+    let res = 0;
+
+    let maxLeft = 0, maxL = new Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+        maxL[i] = maxLeft;
+        maxLeft = Math.max(maxLeft, height[i]);
+    }
+
+    let maxRight = 0;
+    for (let i = n-1; i >= 0; i--) {
+        // maxL array 开始存 min(maxRight, maxL[i]), it is used like minLR[i]
+        maxL[i] = Math.min(maxL[i], maxRight);
+        res += maxL[i] - height[i] > 0 ? maxL[i] - height[i] : 0;
+        maxRight = Math.max(maxRight, height[i]);
+    }
+
+    return res;
+};
--- src/array/two_pointers/TrappingRainWaterSol2.js
+++ src/array/two_pointers/TrappingRainWaterSol2.js
@@ -4,7 +4,7 @@
  * Date        : August 3, 2023
  * Author      : @codingbro
  *
- * meta        : tag-two-pointers, tag-array, tag-math
+ * meta        : tag-two-pointers, tag-array, tag-math, tag-sliding-window
  ***************************************************************************/
 
 /**
@@ -12,5 +12,18 @@
  * @return {number}
  */
 var trap = function(height) {
-    
+    let l = 0, r = height.length - 1, maxLeft = height[l], maxRight = height[r];
+    let res = 0;
+    while (l < r) {
+        if (maxLeft < maxRight) {
+            l++;
+            maxLeft = Math.max(maxLeft, height[l]); // update the max height on the left side
+            res += maxLeft - height[l]; // here maxLeft - height[l] is a non-negative value b/c we already set maxLeft as the max of the two in the prev statement
+        } else {
+            r--;
+            maxRight = Math.max(maxRight, height[r]);
+            res += maxRight - height[r];
+        }
+    }
+    return res;
 };
