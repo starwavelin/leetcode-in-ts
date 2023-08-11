edf7d37902c051022d9cbb0108c11f224af6698b
“starwavelin” <benjaminxianlin@gmail.com>
2023-08-10 21:45:30 -0400

    chore: LC 239 solution 2 polished

--- src/array/sliding_window/SlidingWindowMaximumSol2.js
+++ src/array/sliding_window/SlidingWindowMaximumSol2.js
@@ -13,27 +13,27 @@
  * @return {number[]}
  */
 var maxSlidingWindow = function(nums, k) {
-    const n = nums.length;
     const res = [];
+    const n = nums.length;
+    const q = []; // deque for storing the indices of elements; deque's head contains the index of the max
 
-    const q = [];
-    for (let i = 0; i < n; i++) {
+    for (let l = 0, r = 0; r < n; r++) {
+        // remove smaller number in range k from the end of the deque 
+        // when newly added nums[r] is greater than the number at the end of the deque
+        while (q.length > 0 && nums[r] > nums[q[q.length - 1]]) {
+            q.pop();
+        }
+        q.push(r);
+
         // remove the head of the deque if it is out of range k
-        while (q.length > 0 && q[0] < i - k + 1) {
+        if (l > q[0]) {
             q.shift();
         }
 
-        // remove smaller number in range k from the end of the deque 
-        // when newly added nums[i] is greater than the number at the end of the deque
-        while (q.length > 0 && nums[i] > nums[q[q.length - 1]]) {
-            q.pop();
-        }
-
-        // deque contains the index of elements and deque's head contains the index of the max
-        // res contains the content. When i >= k-1 we start to fill the res
-        q.push(i);
-        if (i >= k - 1) {
+        // When r >= k-1 we start to fill the res        
+        if (r >= k - 1) {
             res.push(nums[q[0]]);
+            l++;
         }
     }
     return res;
@@ -42,3 +42,32 @@ var maxSlidingWindow = function(nums, k) {
 // Tests
 console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3)); // [3, 3, 5, 5, 6, 7]
 console.log(maxSlidingWindow([1], 1)); // [1]
+
+
+
+// Solution 2, old way of writing it, without explicitly use left pointer:
+var maxSlidingWindowSol2 = function(nums, k) {
+    const res = [];
+    const n = nums.length;
+    const q = []; // deque for storing the indices of elements; deque's head contains the index of the max
+
+    for (let r = 0; r < n; r++) {
+        // remove smaller number in range k from the end of the deque 
+        // when newly added nums[r] is greater than the number at the end of the deque
+        while (q.length > 0 && nums[r] > nums[q[q.length - 1]]) {
+            q.pop();
+        }
+        q.push(r);
+
+        // remove the head of the deque if it is out of range k
+        if (r-k+1 > q[0]) {
+            q.shift();
+        }
+
+        // When r >= k-1 we start to fill the res        
+        if (r >= k - 1) {
+            res.push(nums[q[0]]);
+        }
+    }
+    return res;
+};
