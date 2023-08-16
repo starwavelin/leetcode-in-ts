4294347e41d4ba782a2845d8dc714faf7ebd6d30
“starwavelin” <benjaminxianlin@gmail.com>
2023-08-16 13:16:10 -0400

    chore: LC 159 polish solution

--- src/string/sliding_window/LongestSubstringWithAtMostTwoDistinctCharactersSol1.js
+++ src/string/sliding_window/LongestSubstringWithAtMostTwoDistinctCharactersSol1.js
@@ -38,3 +38,28 @@ var lengthOfLongestSubstringTwoDistinct = function(s) {
 
 // Tests
 console.log(lengthOfLongestSubstringTwoDistinct('ecebbbb')) // 5
+
+
+// We can also use Regular Map to handle this problem
+
+var lengthOfLongestSubstringTwoDistinctRegMap = function(s) {
+    let res = 0, count = 0; // the num of distinct chars
+    const map = new Map(); // key - char , val - frequency
+
+    for (let l = 0, r = 0; r < s.length; r++) {
+        if (!map.has(s[r]) || map.get(s[r]) == 0) {
+            count++;
+        }
+        map.set(s[r], (map.get(s[r]) || 0) + 1);
+
+        while (count > 2) {
+            map.set(s[l], map.get(s[l]) - 1);
+            if (map.get(s[l]) == 0) count--;
+            l++;
+        }
+        
+        res = Math.max(res, r-l+1);
+    }
+
+    return res;
+};
