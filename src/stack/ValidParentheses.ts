4b219871c800c17c60db23313fd2e25c57dc767f
“starwavelin” <benjaminxianlin@gmail.com>
2023-08-18 15:15:04 -0400

    feat: LC 20 - add a concise way of writing the solution

--- src/stack/ValidParentheses.ts
+++ src/stack/ValidParentheses.ts
@@ -7,6 +7,32 @@
  * meta        : tag-stack
  ***************************************************************************/
 
+/**
+ * Updated on 8/18/2023
+ * A more concise way of writing it:
+ *  For the comparison part, we check the top element first,
+ *      if it matches, we pop it; otherwise we return false
+ */
+var isValidConciseWay = function (s: string) {
+    const stack = [];
+
+    for (let c of s) {
+        if (c == '(' || c == '[' || c == '{') {
+            stack.push(c);
+        } else {
+            const top = stack[stack.length - 1];
+            if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+                stack.pop();
+            } else return false;
+        }
+    }
+
+    return stack.length == 0;
+};
+
+/**
+ * The solution before
+ */
 function isValid(s: string): boolean {
     const stack = [];
     for (const char of s) {
