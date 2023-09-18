2c3d0e1ff53777ac165353f170b6d9bee7fa9d76
“starwavelin” <benjaminxianlin@gmail.com>
2023-09-18 12:34:07 -0400

    feat: LC 345 sol 1 up

---
+++ src/string/two_pointers/ReverseVowelsOfAStringSol1.js
@@ -0,0 +1,42 @@
+/***************************************************************************
+ * Problem No. : 345
+ * Problem Name: Reverse Vowels of a String
+ * Date        : September 15, 2023
+ * Author      : @codingbro
+ *
+ * meta        : tag-two-pointers, tag-string
+ ***************************************************************************/
+
+/**
+ * Note: 1. In JS, the string type is immutable, so use strArr to manipulate the swap and then 
+ set strArr back to string (use strArr.join(''))
+ 2. Upper case letters should be considered as vowels too.
+ 3. JS doesn't have ArryIndexOutOfBound Exception!!
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    let l = 0, r = s.length-1, n = s.length;
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    const sArr = [...s];
+    while (l < r) {
+        // skip non-vowels on the left side
+        while (l < n && !vowels.includes(sArr[l])) {
+            l++;
+        }
+
+        // skip non-vowels on the right side
+        while (r >= 0 && !vowels.includes(sArr[r])) {
+            r--;
+        }
+
+        if (l < r && vowels.includes(sArr[l]) && vowels.includes(sArr[r])) {
+            [sArr[l], sArr[r]] = [sArr[r], sArr[l]];
+            l++; r--;
+        }
+    }
+    return sArr.join('');
+};
