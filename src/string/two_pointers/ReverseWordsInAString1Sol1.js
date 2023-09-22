f9a65507f7da1a9d8a213f61b281743258238b2c
“starwavelin” <benjaminxianlin@gmail.com>
2023-09-21 22:45:24 -0400

    chore: revisited LC 151

--- src/string/two_pointers/ReverseWordsInAString1Sol1.js
+++ src/string/two_pointers/ReverseWordsInAString1Sol1.js
@@ -7,6 +7,30 @@
  * meta        : tag-two-pointers, tag-string
  ***************************************************************************/
 
+/**
+ * 这题没有特别的技巧,就是记住双指针法和substring的pattern.
+ * 
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWordsJS = function(s) {
+    const n = s.length;
+    let r = n, res = '';
+    for (let l = n-1; l >= 0; l--) {
+        if (s[l] === ' ') r = l;
+        else if (l === 0 || s[l-1] === ' ') {
+            if (res === '') {
+                res += s.substring(l, r);
+            } else {
+                res += ` ${s.substring(l, r)}`;
+            }
+        }
+    }
+
+    return res;
+};
+
+
 class ReverseWordsInAString1 {
     /**
      * Unlike LC 186, this question doesn't require you to in-place modify the input string s.
@@ -14,14 +38,14 @@ class ReverseWordsInAString1 {
      * 方法：双指针一个跟随另一个
      * 使用两个指针从右向左遍历原数组，假定一个指针i,另一个j。
      * j一开始指在s.length位置,i一开始从s.length-1开始。
-     * i逐渐减小，我们让j去跟随i,但只在所指元素是空格的时候才跟随 -- 这就保证j始终指向一个单词的最尾字符+1的位置。
-     * 当i的index为0或者i-1所指的元素为空格时，我们通过substring(i,j)方法截取出想要的单词。
+     *   i逐渐减小，我们让j去跟随i,但只在所指元素是空格的时候才跟随 -- 这就保证j始终指向一个单词的最尾字符+1的位置。
+     *   当i的index为0或者i-1所指的元素为空格时，我们通过substring(i,j)方法截取出想要的单词。
      *
      * 时间复杂度：O(n)
      * 空间复杂度：O(1)
      * @param s
      */
-    reverseWords(s: string): string {
+    reverseWords(s) {
         let res = '';
         const n = s.length;
         let j = n;
