63ea9c72c03ff929a8b5dc1fbbef9e50c03663b4
“starwavelin” <benjaminxianlin@gmail.com>
2023-09-22 22:29:24 -0400

    chore: LC 238 polish solution 2

--- src/math/ProductOfArrayExceptSelfSol1.js
+++ src/math/ProductOfArrayExceptSelfSol1.js
@@ -7,22 +7,23 @@
  * meta        : tag-array, tag-math
  ***************************************************************************/
 
-var productExceptSelf = function(nums) {
+var productExceptSelf = function (nums) {
     const n = nums.length;
     const res = new Array(n);
-    const fwd = new Array(n).fill(1), bwd = new Array(n).fill(1);
-    for (let i=0; i+1 < n; i++) {
-        fwd[i+1] = fwd[i] * nums[i];
+    const fwd = new Array(n).fill(1),
+        bwd = new Array(n).fill(1);
+    for (let i = 0; i + 1 < n; i++) {
+        fwd[i + 1] = fwd[i] * nums[i];
     }
-    for (let i=n-1; i-1 >= 0; i--) {
-        bwd[i-1] = bwd[i] * nums[i];
+    for (let i = n - 1; i - 1 >= 0; i--) {
+        bwd[i - 1] = bwd[i] * nums[i];
     }
-    for (let i=0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         res[i] = fwd[i] * bwd[i];
     }
     return res;
 };
 
 // TEST
-console.log(productExceptSelf([1,2,3,4])); // [24, 12, 8, 6]
-console.log(productExceptSelf([-1,1,0,-3,3])); // [0, 0, 9, 0, 0]
+console.log(productExceptSelf([1, 2, 3, 4])); // [24, 12, 8, 6]
+console.log(productExceptSelf([-1, 1, 0, -3, 3])); // [0, 0, 9, 0, 0]
--- src/math/ProductOfArrayExceptSelfSol2.js
+++ src/math/ProductOfArrayExceptSelfSol2.js
@@ -7,24 +7,24 @@
  * meta        : tag-array, tag-prefix-sum
  ***************************************************************************/
 
-var productExceptSelf = function(nums) {
+var productExceptSelf = function (nums) {
     const n = nums.length;
-    let right = 1;
     const res = new Array(n).fill(1);
+    let right = 1;
 
     // from left to right
-    for (let i=0; i+1<n; i++) {
-        res[i+1] = res[i] * nums[i];
+    for (let i = 1; i < n; i++) {
+        res[i] = res[i - 1] * nums[i - 1];
     }
 
     // from right to left, need to keep updating the var right
-    for (let i=n-1; i >= 0; i--) {
-        res[i] = res[i] * right;
-        right = right * nums[i];
+    for (let i = n - 1; i >= 0; i--) {
+        res[i] *= right;
+        right *= nums[i];
     }
     return res;
 };
 
 // TEST
-console.log(productExceptSelf([1,2,3,4])); // [24, 12, 8, 6]
-console.log(productExceptSelf([-1,1,0,-3,3])); // [0, 0, 9, 0, 0]
+console.log(productExceptSelf([1, 2, 3, 4])); // [24, 12, 8, 6]
+console.log(productExceptSelf([-1, 1, 0, -3, 3])); // [0, 0, 9, 0, 0]
