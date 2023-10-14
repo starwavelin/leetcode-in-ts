0f7e5db9cd3a7fac373631846ba5dd6210602ddc
“starwavelin” <benjaminxianlin@gmail.com>
2023-10-14 16:45:04 -0400

    chore: polish a bit of the README (Q 250-230)

---
+++ src/heap/KClosestPointsToOriginSol2.js
@@ -0,0 +1,22 @@
+/***************************************************************************
+ * Problem No. : 973
+ * Problem Name: K Closest Points to Origin
+ * Date        : September 22, 2023
+ * Author      : @codingbro
+ *
+ * meta        : tag-quick-select
+ ***************************************************************************/
+
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function(points, k) {
+    
+};
+
+// Test kClosest
+console.log(kClosest([[1,3],[-2,2]], 1)); // expect [-2,2]
+console.log(kClosest([[3,3],[5,-1],[-2,4]], 2)); // expect [[-2,4],[3,3]]
+
