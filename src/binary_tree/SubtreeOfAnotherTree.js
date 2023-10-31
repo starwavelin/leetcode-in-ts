5aabef360bbf2b15eebf29afe3df021041b9b2a3
“starwavelin” <benjaminxianlin@gmail.com>
2023-10-31 15:05:51 -0400

    feat: LC 572 up

--- src/binary_tree/SubtreeOfAnotherTree.js
+++ src/binary_tree/SubtreeOfAnotherTree.js
@@ -1,8 +1,46 @@
 /***************************************************************************
  * Problem No. : 572
  * Problem Name: Subtree of Another Tree
- * Date        : October 30, 2023
+ * Date        : October 31, 2023
  * Author      : @codingbro
  *
  * meta        : tag-binary-tree, tag-dfs, tag-meta
  ***************************************************************************/
+
+import { arrayToTree } from './arrayToTree.js';
+
+var isSubtree = function(root, subRoot) {
+    if (!root) {
+        return false;
+    }
+    if (isSameTree(root, subRoot)) {
+        return true;
+    }
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
+
+const isSameTree = (s, t) => {
+    if (!s && !t) {
+        return true;
+    } else if (!t || !s || s.val !== t.val) {
+        return false;
+    }
+    return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+}
+
+
+/**
+ * Tests
+ */
+const tree1 = arrayToTree([3,4,5,1,2]);
+const tree1prime = arrayToTree([4,1,2]);
+console.log(isSubtree(tree1, tree1prime)); // true
+
+const tree2 = arrayToTree([3,4,5,1,2,null,null,null,null,0]);
+const tree2prime = arrayToTree([4,1,2]);
+console.log(isSubtree(tree2, tree2prime)); // false
+
+const tree3 = arrayToTree([1,null,1,null,null,null,1,null,null,null,null,null,null,null,1,null,null,null,null,null,null,null,null,null,null,null,null,null,null,2]);
+// console.log(tree3);
+const tree3prime = arrayToTree([1,2]);
+console.log(isSubtree(tree3, tree3prime)); // true
--- src/binary_tree/arrayToTree.js
+++ src/binary_tree/arrayToTree.js
@@ -13,7 +13,7 @@ class TreeNode {
 }
 
 const arrayToTree = (arr, index = 0) => {
-    if (index >= arr.length || !arr[index]) {
+    if (index >= arr.length || arr[index] === null) {
         return null;
     }
 
@@ -27,17 +27,23 @@ const arrayToTree = (arr, index = 0) => {
     return root;
 }
 
-module.exports = {
+export {
     TreeNode,
     arrayToTree
 }
 
 // Test
 const arr1 = [1, null, 2];
-console.log(arrayToTree(arr1));
+// console.log(arrayToTree(arr1));
 
-const arr2 = [1, null, 2, 3]; // Not legitimate, so the last 3 becomes not printable
-console.log(arrayToTree(arr2));
+const arr2 = [1, null, 2, 3]; // Legitimate in LeetCode but
+    // Not legitimate in my implementation --  the last 3 becomes not printable
+const treeRoot = arrayToTree(arr2);
+// console.log(treeRoot);
+
+// If I want the same outcome as LeetCode
+const arr2prime = [1, null, 2, null, null, 3];
+// console.log(arrayToTree(arr2prime));
 
 // const arr3 = [1, 2, 3, null, null, 4, 5];
 // console.log(arrayToTree(arr3));
