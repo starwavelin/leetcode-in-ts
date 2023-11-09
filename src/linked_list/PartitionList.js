8a786077d4a65e53c1a6c212b79696af6dbe5a50
“starwavelin” <benjaminxianlin@gmail.com>
2023-11-08 19:44:52 -0500

    feat: LC 86 up

--- src/linked_list/PartitionList.js
+++ src/linked_list/PartitionList.js
@@ -7,3 +7,20 @@
  * meta        : tag-linked-list
  ***************************************************************************/
 
+const partition = (head, x) => {
+    const leftDummy = new ListNode(-1), rightDummy = new ListNode(-1);
+    let left = leftDummy, right = rightDummy;
+    while (head) {
+        if (head.val < x) {
+            left.next = head;
+            left = head;
+        } else {
+            right.next = head;
+            right = head;
+        }
+        head = head.next;
+    }
+    right.next = null; // cut the loop
+    left.next = rightDummy.next; // connect left sublist and right sublist
+    return leftDummy.next;
+}
