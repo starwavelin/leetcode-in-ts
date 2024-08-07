57ed5f8d75c3832bf4389a647dc6f34c247c40f5
“starwavelin” <benjaminxianlin@gmail.com>
2024-08-07 17:23:54 -0400

    chore: revisited LC 242

--- src/hash/ValidAnagram.ts
+++ src/hash/ValidAnagram.ts
@@ -8,53 +8,75 @@
  ***************************************************************************/
 
 function isAnagram(s: string, t: string): boolean {
-    if (s.length !== t.length) {
-        return false;
-    }
+  if (s.length !== t.length) {
+    return false;
+  }
 
-    const arrayMap = new Array(26).fill(0);
-    const size = s.length;
-    for (let i = 0; i < size; i++) {
-        arrayMap[s.charCodeAt(i) - 97] += 1; // 97 is the ASCII value of 'a'
+  const arrayMap = new Array(26).fill(0);
+
+  const size = s.length;
+
+  for (let i = 0; i < size; i++) {
+    arrayMap[s.charCodeAt(i) - 97] += 1; // 97 is the ASCII value of 'a'
+  }
+
+  for (let i = 0; i < size; i++) {
+    arrayMap[t.charCodeAt(i) - 97] -= 1;
+    if (arrayMap[t.charCodeAt(i) - 97] < 0) {
+      return false;
     }
-    for (let i = 0; i < size; i++) {
-        arrayMap[t.charCodeAt(i) - 97] -= 1;
-        if (arrayMap[t.charCodeAt(i) - 97] < 0) {
-            return false;
-        }
-    }
-    return true;
+  }
+
+  return true;
 }
 
 function isAnagram2(s: string, t: string): boolean {
-    if (s.length !== t.length) {
-        return false;
-    }
+  if (s.length !== t.length) {
+    return false;
+  }
 
-    const arrayMap = new Array(128).fill(0);
-    const size = s.length;
-    for (let i = 0; i < size; i++) {
-        arrayMap[s.charCodeAt(i)] += 1;
+  const arrayMap = new Array(128).fill(0);
+
+  const size = s.length;
+
+  for (let i = 0; i < size; i++) {
+    arrayMap[s.charCodeAt(i)] += 1;
+  }
+
+  for (let i = 0; i < size; i++) {
+    arrayMap[t.charCodeAt(i)] -= 1;
+    if (arrayMap[t.charCodeAt(i)] < 0) {
+      return false;
     }
-    for (let i = 0; i < size; i++) {
-        arrayMap[t.charCodeAt(i)] -= 1;
-        if (arrayMap[t.charCodeAt(i)] < 0) {
-            return false;
-        }
-    }
-    return true;
+  }
+
+  return true;
 }
 
 function isAnagram3(s: string, t: string): boolean {
-    if (s.length !== t.length) {
-        return false;
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const sArr = Array.from(s).sort();
+  const tArr = Array.from(t).sort();
+
+  for (let i = 0; i < sArr.length; i++) {
+    if (sArr[i] !== tArr[i]) {
+      return false;
     }
-    const sArr = Array.from(s).sort();
-    const tArr = Array.from(t).sort();
-    for (let i = 0; i < sArr.length; i++) {
-        if (sArr[i] !== tArr[i]) {
-            return false;
-        }
-    }
+  }
+
+  return true;
+}
+
+function isAnagram4(s: string, t: string): boolean {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  if ([...s].sort().join('') === [...t].sort().join('')) {
     return true;
+  }
+  return false;
 }
