f643ffd7a2381040ae8ab7429b34e92565402552
“starwavelin” <benjaminxianlin@gmail.com>
2024-08-14 16:57:47 -0400

    chore: revisited LC 347 maxHeap sol

--- README.md
+++ README.md
@@ -242,7 +242,7 @@ Supported list of problem categories for searching,
 | 350 | [Intersection of Two Arrays II](src/array/two_pointers/IntersectionOfTwoArrays2.js) | Easy | Two Pointers, Map | |
 | 349 | [Intersection of Two Arrays](src/array/two_pointers/IntersectionOfTwoArrays.js) | Easy | Two Pointers, Binary Search | |
 | 348 | Design Tic-Tac-Toe $ | Medium | | |
-| 347 | [Top K Frequent Elements](src/heap/TopKFrequentElementsSol1.js), [Solution 2 MaxHeap](src/heap/TopKFrequentElementsSol2.js), [Solution 3 MinHeap](src/heap/TopKFrequentElementsSol3.js), [Solution 4](src/heap/TopKFrequentElementsSol4.js) | Medium | Sort, Heap, Bucket Sort | |
+| 347 | [Top K Frequent Elements](src/heap/TopKFrequentElementsSol1.js), [Solution 2 MaxHeap](src/heap/TopKFrequentElementsSol2.js), [Solution 3 挤压法 MinHeap](src/heap/TopKFrequentElementsSol3.js), [Solution 4](src/heap/TopKFrequentElementsSol4.js) | Medium | Sort, Heap, Bucket Sort | |
 | 346 | Moving Average from Data Stream $ | Easy | | |
 | 345 | [Reverse Vowels of a String](src/string/two_pointers/ReverseVowelsOfAStringSol1.js), [Solution 2](src/string/two_pointers/ReverseVowelsOfAStringSol2.js) | Easy | String, Two Pointers | |
 | 344 | Reverse String | Easy | | |
--- src/heap/TopKFrequentElementsSol2.js
+++ src/heap/TopKFrequentElementsSol2.js
@@ -8,35 +8,36 @@
  ***************************************************************************/
 
 /**
- * Updated on 7/16/2023
- * 
- * Solution Variation - using existing MaxPriorityQueue data structure available on LeetCode,
- *  which is actually from the Version 4.1 of https://github.com/datastructures-js/priority-queue
+ * Updated on 8/14/2024
+ *
+ * Here we can find the supported js libraries used on LeetCode:
+ *   https://support.leetcode.com/hc/en-us/articles/360011833974-What-are-the-environments-for-the-programming-languages
+ *
+ * Solution Variation - using existing PriorityQueue data structure available on LeetCode,
+ *  which is actually from the Version 5 of https://github.com/datastructures-js/priority-queue/blob/v5/README.md
  */
-var topKFrequentMaxPriorityQueue = function(nums, k) {
-    const map = new Map();
-    for (const n of nums) {
-        map.set(n, (map.get(n) || 0) + 1);
-    }
+var topKFrequentPriorityQueue = function (nums, k) {
+  const map = new Map();
+  for (const n of nums) {
+    map.set(n, (map.get(n) || 0) + 1);
+  }
 
-    // build the max heap based on the kvArray, the root is the highest frequency entry of the kvArray
-    const maxHeap = new MaxPriorityQueue(item => item.freq);
-    for (let [key, freq] of map) {
-        maxHeap.enqueue(key, freq); // what we enqueued is .enqueue(element, priority)
-    }
+  // build the max heap using the given library
+  const maxHeap = new PriorityQueue({ compare: (a, b) => b.freq - a.freq });
+  for (let [key, freq] of map) {
+    maxHeap.enqueue({ key, freq });
+  }
 
-    const res = [];
-    for (let i = 0; i < k; i++) {
-        const item = maxHeap.dequeue();
-        res.push(item.element); // what we want is the .element part of the enqueued element
-    }
-    return res;
+  const res = [];
+  for (let i = 0; i < k; i++) {
+    res.push(maxHeap.dequeue().key);
+  }
+
+  return res;
 };
 
-
-
 /**
- * Solution 2: The build max heap apporach
+ * Solution 2: The hand-build max heap apporach
  */
 
 /**
@@ -44,75 +45,78 @@ var topKFrequentMaxPriorityQueue = function(nums, k) {
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {
-    const map = new Map();
-    for (const n of nums) {
-        map.set(n, map.get(n) + 1 || 1);
-    }
-    const kvArray = [...map.entries()];
+var topKFrequent = function (nums, k) {
+  const map = new Map();
+  for (const n of nums) {
+    map.set(n, map.get(n) + 1 || 1);
+  }
+  const kvArray = [...map.entries()];
 
-    // build the max heap based on the kvArray, the root is the highest frequency entry of the kvArray
-    const maxHeap = buildHeap(kvArray);
+  // build the max heap based on the kvArray, the root is the highest frequency entry of the kvArray
+  const maxHeap = buildHeap(kvArray);
 
-    const res = [];
-    for (let i = 0; i < k; i++) {
-        res.push(remove(maxHeap));
-    }
-    return res;
+  const res = [];
+  for (let i = 0; i < k; i++) {
+    res.push(remove(maxHeap));
+  }
+  return res;
 };
 
 const heapify = (array, index) => {
-    const n = array.length;
-    let childIndex;
+  const n = array.length;
+  let childIndex;
 
-    for (; index * 2 < n; index = childIndex) {
-        childIndex = index * 2;
+  for (; index * 2 < n; index = childIndex) {
+    childIndex = index * 2;
 
-        // choose the larger one from two children, and this time array is 2-D
-        if (childIndex !== n-1 && array[childIndex][1] < array[childIndex+1][1]) {
-            childIndex++;
-        }
-
-        if (array[index][1] < array[childIndex][1]) {
-            [array[index], array[childIndex]] = [array[childIndex], array[index]];
-        } else {
-            break;
-        }
+    // choose the larger one from two children, and this time array is 2-D
+    if (childIndex !== n - 1 && array[childIndex][1] < array[childIndex + 1][1]) {
+      childIndex++;
     }
-}
+
+    if (array[index][1] < array[childIndex][1]) {
+      [array[index], array[childIndex]] = [array[childIndex], array[index]];
+    } else {
+      break;
+    }
+  }
+};
 
 const buildHeap = (array) => {
-    const heap = [[undefined, undefined], ...array];
-    const n = heap.length;
-    for (let i = Math.floor(n/2); i >= 1; i--) {
-        heapify(heap, i);
-    }
-    return heap;
-}
+  const heap = [[undefined, undefined], ...array];
+  const n = heap.length;
+  for (let i = Math.floor(n / 2); i >= 1; i--) {
+    heapify(heap, i);
+  }
+  return heap;
+};
 
 const remove = (heap) => {
-    if (heap.length === 1) {
-        throw new Error('Heap is empty!');
-    }
-    
-    const max = heap[1];
-    const last = heap.pop();
-    if (heap.length > 1) {
-        heap[1] = last;
-        heapify(heap, 1);
-    }
+  if (heap.length === 1) {
+    throw new Error('Heap is empty!');
+  }
 
-    return max[0];
-}
+  const max = heap[1];
+  const last = heap.pop();
+  if (heap.length > 1) {
+    heap[1] = last;
+    heapify(heap, 1);
+  }
+
+  return max[0];
+};
 
 /**
  * Testing
  */
-const arr1 = [1,1,1,2,2,3], k1 = 2;
+const arr1 = [1, 1, 1, 2, 2, 3],
+  k1 = 2;
 console.log(topKFrequent(arr1, k1)); // expect [1, 2]
 
-const arr2 = [1,1,1,2,2,3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5], k2 = 3;
+const arr2 = [1, 1, 1, 2, 2, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+  k2 = 3;
 console.log(topKFrequent(arr2, k2)); // expect [5, 1, 3] or similar  5: 9times, 3: 7times, 1: 7times
 
-const arr3 = [4,1,-1,2,-1,2,3], k3 = 2;
+const arr3 = [4, 1, -1, 2, -1, 2, 3],
+  k3 = 2;
 console.log(topKFrequent(arr3, k3)); // expect [-1, 2]   -1: 2times, 2: 2times, others: 1time
