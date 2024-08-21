b2aaf5610a44257f6bc90a0c9d1c3dd90be94526
“starwavelin” <benjaminxianlin@gmail.com>
2024-08-21 17:21:21 -0400

    chore: LC 76, polish Sol 2 and 3

--- src/string/sliding_window/MinimumWindowSubstringSol3.js
+++ src/string/sliding_window/MinimumWindowSubstringSol3.js
@@ -10,60 +10,62 @@
 /**
  * Solution 3
  *  Using the template created by zjh and redtesla
- * 
+ *
  * @param {string} s
  * @param {string} t
  * @return {string}
  */
-var minWindow = function(s, t) {
-    const m = s.length;
-    const map = new Map(); // key: char, val: freq
-    let count = t.length; // count init to be t.length, used for validating if the window string is valid
-    let len = m + 1; // len is the length of the window substring
-    let head = 0; // head - the starting index of the window substring
+var minWindow = function (s, t) {
+  const m = s.length;
 
-    // Init the map
-    for (let c of t) {
-        map.set(c, (map.get(c) || 0) + 1);
+  let head = 0; // head - the starting index of the window substring
+  let len = m + 1; // len is the length of the window substring
+  const map = new Map(); // key: char, val: freq
+  let count = t.length; // count init to be t.length, used for validating if the window string is valid
+
+  // Init the map
+  for (let c of t) {
+    map.set(c, (map.get(c) || 0) + 1);
+  }
+
+  // sliding window template
+  for (let l = 0, r = 0; r < m; r++) {
+    // handle r
+    if (map.get(s[r]) > 0) {
+      count--;
     }
+    map.set(s[r], map.get(s[r]) - 1);
 
-    // sliding window template
-    for (let l = 0, r = 0; r < m; r++) {
-        // handle r
-        if (map.get(s[r]) > 0) {
-            count--;
-        }
-        map.set(s[r], map.get(s[r]) - 1);
+    while (count == 0) {
+      // min window string
+      if (r - l + 1 < len) {
+        len = r - l + 1;
+        head = l;
+      }
 
-        while (count == 0) {
-            // min window string
-            if (r - l + 1 < len) {
-                len = r - l + 1;
-                head = l;
-            }
-
-            // handle l
-            if (map.get(s[l]) == 0) {
-                count++; // make cur substring invalid again
-            }
-            map.set(s[l], map.get(s[l]) + 1);
-            l++;
-        }
+      // handle l
+      map.set(s[l], map.get(s[l]) + 1);
+      if (map.get(s[l]) == 0) {
+        count++; // make cur substring invalid again
+      }
+      l++;
     }
+  }
 
-    return len == m + 1 ? '' : s.substring(head, head + len);    
+  return len == m + 1 ? '' : s.substring(head, head + len);
 };
 
-
 // Tests
-console.log(minWindow("ADOBECK", "ABC")); // ADOBEC
-console.log(minWindow("aa", "aa")); // aa
-console.log(minWindow("BCDEFGHIJKLMNOPQRSTUVWXYZA", "A")); // A
-console.log(minWindow("ADOBECODEBANC", "ABC")); // BANC
+console.log(minWindow('ADOBECK', 'ABC')); // ADOBEC
+console.log(minWindow('aa', 'aa')); // aa
+console.log(minWindow('BCDEFGHIJKLMNOPQRSTUVWXYZA', 'A')); // A
+console.log(minWindow('ADOBECODEBANC', 'ABC')); // BANC
 
-const s1 = "zfvdiuibotzsqrpgfnbfwudczyruzvuyaselommcfmuxdmgkzhpydsafttzsowrrovccjqhpcdohpurpeiphdrmwkooykfracvemmldqpragmtxqcmxfdmbnapomxfmzdqlpeofvghbubzkdnjirxlgxaujzcxzfqmuudbrllsfmtrpjczaakgzmdlofinkybgugjlrugygzrxiuwkwitvxwilbranrbvmigzbbfcjhthrpfclqxjntrawkajcdgqlmpppxrzemivcpzpfwauruuneuyiyeylrqagnthrgpokhozmmaheudryysjywhjpzmymhhfnxwxemzsyzbcvfwvfoedmoocnccckjjzifzoryhqxkuurspwgubtkqxxuzbeilersdhkdoccbywsrlpxhssriprdqujzhnsaszmvqoxrotjfhafqtxkdpbifvsgfhafccr"
-const t1 = "xshxlvswdb"
+const s1 =
+  'zfvdiuibotzsqrpgfnbfwudczyruzvuyaselommcfmuxdmgkzhpydsafttzsowrrovccjqhpcdohpurpeiphdrmwkooykfracvemmldqpragmtxqcmxfdmbnapomxfmzdqlpeofvghbubzkdnjirxlgxaujzcxzfqmuudbrllsfmtrpjczaakgzmdlofinkybgugjlrugygzrxiuwkwitvxwilbranrbvmigzbbfcjhthrpfclqxjntrawkajcdgqlmpppxrzemivcpzpfwauruuneuyiyeylrqagnthrgpokhozmmaheudryysjywhjpzmymhhfnxwxemzsyzbcvfwvfoedmoocnccckjjzifzoryhqxkuurspwgubtkqxxuzbeilersdhkdoccbywsrlpxhssriprdqujzhnsaszmvqoxrotjfhafqtxkdpbifvsgfhafccr';
+const t1 = 'xshxlvswdb';
 console.log(minWindow(s1, t1)); // "bywsrlpxhssriprdqujzhnsaszmvqox"
 
-const s2 = 'acedfecbd', t2 = 'cef';
+const s2 = 'acedfecbd',
+  t2 = 'cef';
 console.log(minWindow(s2, t2)); // 'fec'
