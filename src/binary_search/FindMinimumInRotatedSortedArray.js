66b96c4f6e487dc70709623908760d9a14395989
“starwavelin” <benjaminxianlin@gmail.com>
2024-08-22 11:04:19 -0400

    chore: revisited LC 153

--- src/binary_search/FindMinimumInRotatedSortedArray.js
+++ src/binary_search/FindMinimumInRotatedSortedArray.js
@@ -7,19 +7,22 @@
  * meta        : tag-binary-search
  ***************************************************************************/
 
-var findMin = function(nums) {
-    const n = nums.length;
-    if (n === 1 || nums[0] < nums[n-1]) {
-        return nums[0];
+var findMin = function (nums) {
+  const n = nums.length;
+  if (n === 1 || nums[0] < nums[n - 1]) {
+    return nums[0];
+  }
+  let l = 0,
+    r = n - 1;
+  while (l + 1 < r) {
+    const mid = l + Math.floor((r - l) / 2);
+    if (nums[l] < nums[mid]) {
+      // 当前从l到mid是递增的
+      l = mid;
+    } else {
+      r = mid;
     }
-    let l = 0, r = n-1;
-    while (l + 1 < r) {
-        const mid = l + Math.floor( (r-l)/2 );
-        if (nums[mid] > nums[l]) { // 当前从l到mid是递增的
-            l = mid;
-        } else {
-            r = mid;
-        }
-    }
-    return nums[r];
+  }
+
+  return nums[r];
 };
