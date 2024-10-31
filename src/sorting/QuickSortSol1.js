51142f28198cfe7a0c456418cdf1dcefd5153854
“starwavelin” <benjaminxianlin@gmail.com>
2024-10-31 19:44:00 -0400

    feat: Quick Sort based on Array Partition 1 is done

--- src/sorting/QuickSortSol1.js
+++ src/sorting/QuickSortSol1.js
@@ -1,7 +1,7 @@
 /***************************************************************************
  * Problem No. : 10007
  * Problem Name: Quick Sort
- * Date        : November 13, 2023
+ * Date        : October 31, 2024
  * Author      : @codingbro
  * Notes       :
  * 	Scenario:
@@ -12,3 +12,52 @@
         - What is the value range of each number? [-100, 100]
  * meta        : tag-sort, tag-two-pointers, tag-array
  ***************************************************************************/
+
+/**
+ * This version of Quick Sort is based on Partition Array Method 1 -
+ * using a left pointer chasing after a right pointer
+ *
+ * @param {*} nums
+ * @returns
+ */
+
+const sort = (nums) => {
+  quickSort(nums, 0, nums.length - 1);
+  return nums;
+};
+
+const quickSort = (nums, left, right) => {
+  if (left < right) {
+    const partitionIndex = partition(nums, left, right);
+    quickSort(nums, left, partitionIndex - 1);
+    quickSort(nums, partitionIndex + 1, right);
+  }
+};
+
+const partition = (nums, low, high) => {
+  const pivotIndex = high;
+  let l = low,
+    r = low;
+
+  while (r < high) {
+    // Move elements less than pivot to the front
+    if (nums[r] < nums[pivotIndex]) {
+      if (r !== l) {
+        [nums[r], nums[l]] = [nums[l], nums[r]];
+      }
+
+      l++;
+    }
+
+    r++;
+  }
+
+  // swap the num in position l and pivotIndex
+  [nums[l], nums[pivotIndex]] = [nums[pivotIndex], nums[l]];
+
+  return l;
+};
+
+module.exports = {
+  sort
+};
