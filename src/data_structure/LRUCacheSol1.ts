e64c10426f2f3fae5aba7b0c39195f72aa5f8fcb
“starwavelin” <benjaminxianlin@gmail.com>
2024-11-05 16:38:55 -0500

    chore: modify a dummy file

--- src/data_structure/LRUCacheSol1.ts
+++ src/data_structure/LRUCacheSol1.ts
@@ -1,1 +1,12 @@
-lsdjglkasgklajsljg;
+/***************************************************************************
+ * Problem No. : 146
+ * Problem Name: LRU Cache
+ * Date        : November 5, 2024
+ * Author      : @codingbro
+ *
+ * meta        : tag-data-structure, tag-map, tag-linked-list
+ ***************************************************************************/
+
+/**
+ * This is a dummy file, see the corresponding .js file for a solution
+ */
